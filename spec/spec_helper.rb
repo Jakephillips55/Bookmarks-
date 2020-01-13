628ccab4339658a64738ee8784861dafecf91b55
Jakephillips55 <Jakep629@gmail.com>
2020-01-13 16:34:55 +0000

    slight syntax edit still bugging

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 # Set the environment to "test"
 
-require_relative './setup_test_database'
 
-ENV['RACK_ENV'] = 'test'
 ENV['ENVIRONMENT'] = 'test'
 
 # Bring in the contents of the `app.rb` file. The below is equivalent to: require_relative '../app.rb'
@@ -12,16 +10,16 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require_relative './setup_test_database'
 
 # Tell Capybara to talk to BookmarkManager
 Capybara.app = BookmarkManager
 
-ENV['ENVIRONMENT'] = 'test'
-
 RSpec.configure do |config|
+
   config.before(:each) do
     setup_test_database
-  end
+
 end
 
 
@@ -128,3 +126,4 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+end 
